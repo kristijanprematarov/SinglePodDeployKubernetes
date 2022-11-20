02ca69f839ee0c65e8da73beb8c10a05e5504be2
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-11-20 18:38:10 +0100

    Add project files.

---
+++ TestApp.WebApi/Models/Joke.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp.WebApi.Models
+{
+    public class Joke
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ TestApp.WebUI/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TestApp.WebApi.Models;
+
+namespace TestApp.WebUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+
+                request.RequestUri = new Uri("http://localhost:81/api/jokes");
+                var response = await client.SendAsync(request);
+
+                var joke = await response.Content.ReadAsStreamAsync();
+
+                ViewData["Joke"] = await JsonSerializer.DeserializeAsync<Joke>(joke, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                return Page();
+            }
+        }
+    }
+}
